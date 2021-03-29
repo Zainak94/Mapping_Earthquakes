4265190e5827d7a6748e237c8e45fe6f744a9940
Zainak94 <zainak94@gmail.com>
2021-03-29 00:15:48 -0400

    updates in final challenge

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -54,11 +54,7 @@ L.control.layers(baseMaps, overlays).addTo(map);
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data)
-{
-  // This function returns the style data for each of the earthquakes we plot on
-  // the map. We pass the magnitude of the earthquake into two separate functions
-  // to calculate the color and radius.
-  function styleInfo(feature) {
+{function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
@@ -81,7 +77,6 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   }
 
   // This function determines the radius of the earthquake marker based on its magnitude.
-  // Earthquakes with a magnitude of 0 were being plotted with the wrong radius.
   function getRadius(magnitude) {
     if (magnitude === 0) {
       return 1;
@@ -91,14 +86,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
-      // We turn each feature into a circleMarker on the map.
       pointToLayer: function(dummy, latlng) {
           return L.circleMarker(latlng);
         },
       style: styleInfo,
-      // We set the style for each circleMarker using our styleInfo function.
-      // We create a popup for each circleMarker to display the magnitude and location of the earthquake
-      // after the marker has been created and styled.
       onEachFeature: function(feature, layer) {
       layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
       }
@@ -129,11 +120,11 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geoj
 
       // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
       function getColor(magnitude) {
-        if (magnitude > 5) {return "#ea2c2c"; //red //return "#BD40AE"; pink
+        if (magnitude > 5) {return "#ea2c2c"; 
         }
-        else if (magnitude > 4) {return "#ea822c"; //orange //return "#8A3AA6"; purple
+        else if (magnitude > 4) {return "#ea822c";
         }
-        else {return "#3A56A6"; //yellow //return "#3A56A6"; blue
+        else {return "#3A56A6"; 
         }  
       }
 
